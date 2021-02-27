5b25385e04ef03d553a2e50bcbf09a07704a28d1
Jorge Tadeo <jorge_tadeo@icloud.com>
2021-02-26 16:34:58 -0800

    added the app component that will house the web app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './components/App'
 
 ReactDOM.render(
-    <p>Hello</p>,
+    <App />,
     document.getElementById('root')
   );
