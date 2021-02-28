afdd4fed7613b00e57213250250e888f3b4733e7
Jorge Tadeo <jorge_tadeo@icloud.com>
2021-02-27 18:25:27 -0800

    Added links to the logos in Social, set the max width of the info section of the About component and added a pictures folder

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,8 +5,8 @@ function Social () {
 
     return(
         <div className="Social">
-            <FontAwesomeIcon className="facebook" icon={['fab', 'facebook-f']} size="lg" />
-            <FontAwesomeIcon className="instagram" icon={['fab', 'instagram']} size="lg"/>
+            <a href="https://www.facebook.com/"><FontAwesomeIcon className="facebook" icon={['fab', 'facebook-f']} size="lg" /></a>
+            <a href="https://www.instagram.com/"><FontAwesomeIcon className="instagram" icon={['fab', 'instagram']} size="lg"/></a>
             <FontAwesomeIcon icon="envelope"  size="lg" />
         </div>
     );
